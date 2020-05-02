1ef74d9af06e41234f9afbb93cade0208acabbf0
theViz343 <vishwesh103@gmail.com>
2020-05-02 21:53:43 +0530

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "Please wait...."
+python3 -m venv venv1
+source venv1/bin/activate
+pip3 install django
+pip3 install urllib3
+pip3 install requests
+deactivate
+cat <<EOF > config1.json
+{
+"client_id" : "your_client_id_here",
+"client_secret" : "your_client_secret_here",
+}
+EOF
