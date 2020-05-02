3865a9721a01a1b5a270a79cd31e6c555c262236
theViz343 <44305101+theViz343@users.noreply.github.com>
2020-05-02 22:29:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip3 install django
 pip3 install urllib3
 pip3 install requests
 deactivate
-cat <<EOF > config1.json
+cat <<EOF > config.json
 {
 "client_id" : "your_client_id_here",
 "client_secret" : "your_client_secret_here",
